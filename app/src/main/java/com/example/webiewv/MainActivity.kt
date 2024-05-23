a89a64236d1701a8fd2f9860bd74d67367ea654f
Sagar Powar <sagarpowar1993@gmail.com>
2024-05-23 11:38:56 +0530

    first add

---
+++ app/src/main/java/com/example/webiewv/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.webiewv
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.webiewv.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    setContentView(binding.root)
+    }
+}
+
